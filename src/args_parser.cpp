726d0b32e55f5b65c208c04920b97900f8f752a8
Avishay Spitzer <a_spitzer@apple.com>
2024-08-04 18:21:07 +0300

    args_parser - read paths

--- src/args_parser.cpp
+++ src/args_parser.cpp
@@ -3,17 +3,38 @@
 #include "args_parser.h"
 #include "logger.h"
 
-ArgsParseResults ArgsParser::parse(int argc, char* argv[]) {
-    bool enable_visualization = false;
-    if (argc < 2) {
-        logger.log(FATAL, "requires at least one argument - input file, got 0");
-    } else if (argc == 3) {
-        if (std::string(argv[2]) != "--visualize") {
-            logger.log(FATAL, std::format("Unknown argument: {}", std::string(argv[2])));
-        } else {
-            enable_visualization = true;
-        }
+ArgsParseResults ArgsParser::parse(int argc, char* argv[]) const {
+    using string = std::string;
+    if (argc != 3) {
+        logger.log(FATAL, std::format("Expected 2 arguments for the program got {}", argc - 1));
     }
-    std::string input_file_path = argv[1];
-    return ArgsParseResults{input_file_path, enable_visualization};
+    string arg1 = string(argv[1]);
+    string arg2 = string(argv[2]);
+
+    return extractPaths(arg1, arg2);
+}
+
+ArgsParseResults ArgsParser::extractPaths(const std::string& input1, const std::string& input2) const {
+    std::string housePath;
+    std::string algoPath;
+
+    const std::string housePrefix = "-house_path=";
+    const std::string algoPrefix = "-algo_path=";
+
+    // parse input1
+    if (input1.find(housePrefix) == 0) {
+        housePath = input1.substr(housePrefix.size());
+    } else if (input1.find(algoPrefix) == 0) {
+        algoPath = input1.substr(algoPrefix.size());
+    }
+
+    // parse input2
+    if (input2.find(housePrefix) == 0) {
+        housePath = input2.substr(housePrefix.size());
+    } else if (input2.find(algoPrefix) == 0) {
+        algoPath = input2.substr(algoPrefix.size());
+    }
+    logger.log(INFO, std::format("houses path: {}", housePath));
+    logger.log(INFO, std::format("algorithms path: {}", algoPath));
+    return ArgsParseResults{housePath, algoPath};
 }
--- src/args_parser.h
+++ src/args_parser.h
@@ -2,11 +2,12 @@
 #include <string>
 
 struct ArgsParseResults {
-    std::string input_file_path;
-    bool enable_visualization;
+    std::string houses_path;
+    std::string algos_path;
 };
 
 class ArgsParser {
+    ArgsParseResults extractPaths(const std::string& input1, const std::string& input2) const;
 public:
-    ArgsParseResults parse(int argc, char* argv[]);
+    ArgsParseResults parse(int argc, char* argv[]) const;
 };
